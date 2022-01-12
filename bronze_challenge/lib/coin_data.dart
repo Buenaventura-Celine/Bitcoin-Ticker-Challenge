c741ec555d0fc53cf7945d3b8f999b159a7d4e58
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-01-12 23:33:20 +0800

    getCoinData() method

--- bronze_challenge/lib/coin_data.dart
+++ bronze_challenge/lib/coin_data.dart
@@ -33,8 +33,18 @@ const List<String> cryptoList = [
 ];
 
 const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
-const apiKey = 'YOUR-API-KEY-HERE';
+const apiKey = '45EA5360-A444-4056-B3F0-491AB005156D';
 
 class CoinData {
   //TODO: Create your getCoinData() method here.
+  Future getCoinData() async {
+    http.Response response =
+        await http.get(Uri.parse('$coinAPIURL/BTC/USD?apiKey=$apiKey'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
 }
