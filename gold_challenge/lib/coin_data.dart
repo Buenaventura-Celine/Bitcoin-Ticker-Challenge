3ac973542c289c734236a19d0d403381d0bf04ea
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-01-13 00:30:11 +0800

    Use for loopp hereee to loop through the cryptoList

--- gold_challenge/lib/coin_data.dart
+++ gold_challenge/lib/coin_data.dart
@@ -38,15 +38,17 @@ class CoinData {
   Future getCoinData(String selectedCurrency) async {
     //TODO 4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
     //TODO 5: Return a Map of the results instead of a single value.
-    String requestURL = '$coinAPIURL/BTC$selectedCurrency';
-    http.Response response = await http.get(requestURL);
-    if (response.statusCode == 200) {
-      var decodedData = jsonDecode(response.body);
-      var lastPrice = decodedData['rate'];
-      return lastPrice;
-    } else {
-      print(response.statusCode);
-      throw 'Problem with the get request';
+    for (String crypto in cryptoList) {
+      String requestURL = '$coinAPIURL/$crypto/$selectedCurrency';
+      http.Response response = await http.get(requestURL);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        var lastPrice = decodedData['rate'];
+        return lastPrice;
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
     }
   }
 }
